4f01af5017e9d6cd7f1d166699b46d226ccceeb1
jeremy duhem <redkardss@gmail.com>
2024-05-15 16:27:04 +0200

    modifexo6

--- exo6/test6.js
+++ exo6/test6.js
@@ -1,38 +1,30 @@
-let a = 0;
-let b = 0;
-let c = 0;
-let d = 0;
-let e = 0;
-let message1 = "entrer la 1ère note";
-let message2 = "entrer la 2ème note";
-let message3 = "entrer la 3ème note";
-let message4 = "entrer la 4ème note";
-let message5 = "entrer la 5ème note";
-let nombres = [a, b, c, d, e];
+let = a = prompt("entrer la 1ère note");
+let = b = prompt("entrer la 2ème note");
+let = c = prompt("entrer la 3ème note");
+let = d = prompt("entrer la 4ème note");
+let = e = prompt("entrer la 5ème note");
 
-let filtre = function (nombre) {
-  return Math.round(nombre);
-};
+console.log(a, b, c, d, e);
 
-function question() {
-  a = prompt(message1);
-  b = prompt(message2);
-  c = prompt(message3);
-  d = prompt(message4);
-  e = prompt(message5);
+a = Number(a);
+b = Number(b);
+c = Number(c);
+d = Number(d);
+e = Number(e);
+let moyenne = (a + b + c + d + e) / 5;
 
-  console.log(a, b, c, d, e);
+console.log(moyenne);
+
+if (moyenne < 8) {
+  alert("moyenne inférieur à 8");
+} else if (moyenne > 8 && moyenne < 10) {
+  alert("moyenne inférieur à 10");
+} else if (moyenne < 10 && moyenne < 12) {
+  alert("moyenne inférieur à 12");
 }
-// function moyenne() {
-//   a + b + c + d + e;
+// else if (moyenne<14) {
+//   alert("moyenne inférieur à 14");
 // }
-Number(a);
-
-console.log(a);
-
-// let x = Number(a) + Number(b) + Number(c) + Number(d) + Number(e);
-
-question();
-//moyenne();
-//console.log(moyenne);
-// console.log(x);
+// else  (moyenne<16) {
+//   alert("moyenne inférieur à 16");
+// }
